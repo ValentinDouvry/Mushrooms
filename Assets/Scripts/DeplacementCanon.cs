45fb331087582d850d0d3918badd0470513582c2
Unknown <1643216@cgmatane.qc.ca>
2018-11-20 21:16:56 -0500

    Script deplacement cannon final

--- Assets/Scripts/DeplacementCanon.cs
+++ Assets/Scripts/DeplacementCanon.cs
@@ -1,16 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeplacementCanon : MonoBehaviour {
-	public float sensibilite = 150.0f;
-	public GameObject cameraPivot; 
+	public GameObject barrel;
 	
+	public float sensibilite = 150.0f;
+
+	public float angleMaxTourelle = 70,
+				angleMaxPosTourrelle,
+				angleMaxNegTourrelle,
+				angleMaxCannon = 45,
+				angleMaxPosCannon,
+				angleMaxNegCannon;
+	
+	private float angleXTarget, 
+				angleYTarget, 
+				angleXActuelle, 
+				angleYActuelle;
+	
+	void Start()
+	{
+		angleMaxPosTourrelle = angleMaxTourelle;
+		angleMaxNegTourrelle = -angleMaxTourelle;
+		angleMaxNegCannon = -angleMaxCannon;
+		angleMaxPosCannon = angleMaxCannon;
+	}
+
 	void Update () {
 		float y = Input.GetAxis("Horizontal") * Time.deltaTime * sensibilite; 
-		float x = Input.GetAxis("Mouse Y") * Time.deltaTime * sensibilite; 
- 
-		cameraPivot.transform.Rotate(x, 0, 0, Space.Self); 
-		transform.Rotate(0, y, 0); 
-	} 
+		float x = Input.GetAxis("Mouse Y") * Time.deltaTime * sensibilite;
+
+		angleYActuelle = transform.rotation.eulerAngles.x;
+		angleXActuelle = barrel.transform.rotation.eulerAngles.y;
+
+		angleYTarget += y;
+		angleYTarget = Mathf.Clamp(angleYTarget, angleMaxNegTourrelle, angleMaxPosTourrelle);
+		transform.rotation = Quaternion.Euler(0, angleYTarget, 0);
+
+		angleXTarget += x;
+		angleXTarget = Mathf.Clamp(angleXTarget, angleMaxNegCannon, angleMaxPosCannon);
+		barrel.transform.rotation = Quaternion.Euler(angleXTarget, angleYTarget, 0);
+		
+	}
 }
