ea4a02940380ed57ea19bef843efe3731a638896
Unknown <1643216@cgmatane.qc.ca>
2018-11-20 19:21:10 -0500

    Correction rotation plus naturelle

--- Assets/Scripts/DeplacementCanon.cs
+++ Assets/Scripts/DeplacementCanon.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeplacementCanon : MonoBehaviour {
-	public float sensibilite = 150.0f; 
-	 
-	void Update () { 
-		float x = Input.GetAxis("Horizontal") * Time.deltaTime * sensibilite; 
-		float y = Input.GetAxis("Mouse Y") * Time.deltaTime * sensibilite; 
+	public float sensibilite = 150.0f;
+	public GameObject cameraPivot; 
+	
+	void Update () {
+		float y = Input.GetAxis("Horizontal") * Time.deltaTime * sensibilite; 
+		float x = Input.GetAxis("Mouse Y") * Time.deltaTime * sensibilite; 
  
-		transform.Rotate(0, x, 0); 
-		transform.Rotate(y, 0, 0); 
+		cameraPivot.transform.Rotate(x, 0, 0, Space.Self); 
+		transform.Rotate(0, y, 0); 
 	} 
 }
