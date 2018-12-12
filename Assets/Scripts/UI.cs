02615c79aa6adf631922404cc12d3cbe3e8756c2
Unknown <sedrickidd@hotmail.com>
2018-12-11 19:58:14 -0500

    Ajout du script de UI avec le timer descendent

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+    public float time = 60f;
+    public Text textTemps;
+
+    private string timeMinutes;
+    private string timeSeconds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        refreshTimer();
+        reduceTime();
+	}
+
+    void reduceTime()
+    {
+        time -= Time.deltaTime;
+    }
+    void refreshTimer()
+    {
+        timeMinutes = Mathf.Floor(time / 60).ToString("00");
+        timeSeconds = Mathf.RoundToInt(time % 60).ToString("00");
+        textTemps.text = timeMinutes + ":" + timeSeconds;
+    }
+}
