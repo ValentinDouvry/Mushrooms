138b4238443c4080965cd8e0da341fab5d3b526c
Douvry <valentin.jabba@gmail.com>
2019-07-20 05:21:44 -0400

    Ajout Menu principal fonctionnel , sans illustrations

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void Play()
+    {
+        // Get next scene in project build settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        // Only works in build so only logs in editor
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
